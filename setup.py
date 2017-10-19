014c1fbcc7185b37cdb4155369aaaab3f3c093c0
Matthew Sullivan <sullivan.matt@gmail.com>
2017-10-19 14:07:06 -0500

    Bump version to 2.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='splunk_handler',
-    version='2.0.5',
+    version='2.0.7',
     license='MIT License',
     description='A Python logging handler that sends your logs to Splunk',
     long_description=open('README.md').read(),
